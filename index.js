f61f0aa8eab0e4d412e3dce2aca1d43f6a377eea
Matthew Freeman <matthew.freeman821@gmail.com>
2021-01-16 16:38:17 -0500

    Attempts to remove paragraphs when new search conducted. Currently keeps one element from previous search.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const countryInput = document.getElementById('country');
 const header = document.getElementById('title');
 const input = document.getElementById('input');
 const dataDisplay = document.getElementById('data');
+const para = document.getElementsByName('p');
 
 //Initializes the variable used to search and display
 let country, countryData;
@@ -18,16 +19,25 @@ countryInput.addEventListener("change", () => {
 async function fetchData(countryName){
   const res = await fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
   countryData = await res.json();
-  for(let i=0; i<displayData.length; i++){
-    dataDisplay.appendChild(createDataItem(countryData[0][displayData[i]]));
+  if(dataDisplay.childElementCount === 0){
+    for(let i=0; i<displayData.length; i++){
+      dataDisplay.appendChild(createDataItem(displayData[i] + ':' + countryData[0][displayData[i]]));
+    } 
+  } else {
+      for(let j=0; j<dataDisplay.childElementCount; j++){
+        dataDisplay.removeChild(dataDisplay.childNodes[j]);
+      }
+      for(let i=0; i<displayData.length; i++){
+        dataDisplay.appendChild(createDataItem(displayData[i] + ':' + countryData[0][displayData[i]]));
+      } 
+    }
   };
-};
 
 //Funciton to create paragraph element for data display
 function createDataItem(info){
   let p = document.createElement('p');
   p.textContent = info;
-  p.className = 'is-flex m-6';
+  p.className = 'is-flex m-4';
   return p;
 }
 
